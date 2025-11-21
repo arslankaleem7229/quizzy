35557410f61250a4463dc42ea84fcfe7770388a6
arslankaleem7229 <arslankaleem7229@gmail.com>
2025-11-21 18:04:09 +0000

    creating setting page and routing

---
+++ app/settings/NotificationSetting.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { useState } from "react";
+import SettingEditButtonRow from "./components/SettingEditButtonRow";
+import { HiOutlineChevronDown, HiOutlineEnvelope } from "react-icons/hi2";
+
+const NotificationSetting = () => {
+  const [studyUpdatesOpen, setStudyUpdatesOpen] = useState(true);
+  const [quizletUpdatesOpen, setQuizletUpdatesOpen] = useState(false);
+  return (
+    <section className="space-y-4">
+      <h2 className="text-white font-medium">Notifications</h2>
+      <div className="rounded-xl border-2 border-gray-700">
+        <SettingEditButtonRow
+          label={"Personalised study updates"}
+          value={""}
+          onAction={() => setStudyUpdatesOpen((prev) => !prev)}
+          enableTopBorder={false}
+          enableBottomBorder={false}
+        >
+          <HiOutlineChevronDown
+            className={`h-5 w-5 transition text-white ${
+              studyUpdatesOpen ? "rotate-180" : ""
+            }`}
+          />
+        </SettingEditButtonRow>
+
+        {studyUpdatesOpen && (
+          <div className="">
+            <SettingEditButtonRow label={"Streaks and badges"} value={""}>
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-(--primary) text-white transition">
+                <HiOutlineEnvelope className="h-5 w-5" />
+              </div>
+            </SettingEditButtonRow>
+            <SettingEditButtonRow label={"Study reminders"} value={""}>
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-(--primary) text-white transition">
+                <HiOutlineEnvelope className="h-5 w-5" />
+              </div>
+            </SettingEditButtonRow>
+          </div>
+        )}
+      </div>
+      <div className="rounded-xl border-2 border-gray-700">
+        <SettingEditButtonRow
+          label={"Quizlet updates"}
+          value={""}
+          onAction={() => setQuizletUpdatesOpen((prev) => !prev)}
+          enableTopBorder={false}
+        >
+          <HiOutlineChevronDown
+            className={`h-5 w-5 transition text-white ${
+              quizletUpdatesOpen ? "rotate-180" : ""
+            }`}
+          />
+        </SettingEditButtonRow>
+
+        {quizletUpdatesOpen && (
+          <div className="">
+            <SettingEditButtonRow
+              enableTopBorder={false}
+              label={"New features, tips and study challenges"}
+              value={""}
+            >
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-(--primary) text-white transition">
+                <HiOutlineEnvelope className="h-5 w-5" />
+              </div>
+            </SettingEditButtonRow>
+            <SettingEditButtonRow
+              label={"Sales, giveaways and promotions"}
+              value={""}
+            >
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-(--primary) text-white transition">
+                <HiOutlineEnvelope className="h-5 w-5" />
+              </div>
+            </SettingEditButtonRow>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default NotificationSetting;
---
+++ app/settings/components/SettingDropDownRow.tsx
@@ -0,0 +1,39 @@
+import { ChangeEventHandler, ReactNode } from "react";
+import { HiOutlineChevronDown } from "react-icons/hi2";
+
+const SettingDropDownRow = ({
+  label,
+  value,
+  actionLabel,
+  onAction,
+  children,
+}: {
+  label: string;
+  value: string;
+  actionLabel?: string;
+  onAction: ChangeEventHandler<HTMLSelectElement>;
+  children?: ReactNode;
+}) => {
+  return (
+    <div className="flex flex-row gap-2 last:border-none border-b-2 border-gray-700 pl-6 pr-10 py-6 items-center justify-between ">
+      <div>
+        <p className="font-medium"> {label} </p>
+      </div>
+      <div className="inline-flex gap-2 items-center rounded-full bg-[#2E3856] pr-6 text-sm font-semibold transition">
+        <select
+          value={value}
+          name={actionLabel}
+          onChange={onAction}
+          // TODO: Fix this
+          className="w-full bg-transparent text-white outline-none pl-6 py-3.5 appearance-none"
+        >
+          {children}
+          {/* {actionLabel} */}
+        </select>
+        <HiOutlineChevronDown className="h-4 w-4" />
+      </div>
+    </div>
+  );
+};
+
+export default SettingDropDownRow;
