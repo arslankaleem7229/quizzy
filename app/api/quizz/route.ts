1eb08cbce2218d1d4f51bae87021cde7303e3de3
arslankaleem7229 <arslankaleem7229@gmail.com>
2025-12-10 22:45:57 +0000

    Completed Quizz APIs
    - Fetching all quizz
    - Pagination added
    - Upsert query for quizz

--- app/api/quizz/route.ts
+++ app/api/quizz/route.ts
@@ -1,16 +1,69 @@
 import prisma from "@/prisma/client";
 import { verifyApiAuth } from "@/lib/utils/verifyToken";
 import { NextRequest, NextResponse } from "next/server";
+import Error from "next/error";
+import { ReturnQuizzesOnly } from "@/lib/types/prisma";
 
 export async function GET(request: NextRequest) {
   const auth = await verifyApiAuth(request);
   if (!auth.authorized) return auth.response;
 
-  const quizz = await prisma.quizz.findMany({
-    include: { questions: true },
-  });
+  const { searchParams } = new URL(request.url);
 
-  if (!quizz)
-    return NextResponse.json({ error: "No Quizzes found" }, { status: 404 });
-  return NextResponse.json(quizz, { status: 201 });
+  const cursor = searchParams.get("cursor");
+  const mode = searchParams.get("mode") || "mini";
+  const limit = Number(
+    searchParams.get("limit") ?? (mode === "mini" ? "5" : "20")
+  );
+
+  try {
+    const quizzes = await prisma.quizz.findMany({
+      take: limit,
+      skip: cursor ? 1 : 0,
+      cursor: cursor ? { id: cursor } : undefined,
+
+      include: {
+        sets: {
+          select: {
+            title: true,
+            description: true,
+            language: true,
+            _count: { select: { userQuizzAttempts: true, questions: true } },
+          },
+        },
+        createdBy: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+            email: true,
+            type: true,
+          },
+        },
+      },
+    });
+
+    if (!quizzes)
+      return NextResponse.json({ error: "No Quizzes found" }, { status: 404 });
+
+    const nextCursor =
+      quizzes.length === limit ? quizzes[quizzes.length - 1].id : null;
+
+    return NextResponse.json(
+      {
+        quizzes,
+        pagination: {
+          limit: limit,
+          cursor: cursor ?? null,
+          nextCursor: nextCursor,
+          hasMore: nextCursor != null,
+        },
+      } satisfies ReturnQuizzesOnly,
+      { status: 200 }
+    );
+  } catch (error) {
+    const err = error as Error;
+    console.log(error);
+    return NextResponse.json({ error: err }, { status: 400 });
+  }
 }
