2391810f19bcf0f1fe3727192dd50f928f19ae65
arslankaleem7229 <arslankaleem7229@gmail.com>
2025-12-12 19:20:43 +0000

    fetching specific quizz

--- app/api/quizz/[id]/route.ts
+++ app/api/quizz/[id]/route.ts
@@ -1,20 +1,42 @@
 import prisma from "@/prisma/client";
 import { verifyApiAuth } from "@/lib/utils/verifyToken";
 import { NextRequest, NextResponse } from "next/server";
+import { ReturnSpecificQuizz } from "@/lib/types/prisma";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
   const auth = await verifyApiAuth(request);
   if (!auth.authorized) return auth.response;
 
   const quizz = await prisma.quizz.findUnique({
-    where: { id: params.id },
-    include: { questions: true },
+    where: { id: id },
+    include: {
+      sets: {
+        where: { language: "en" },
+        include: {
+          questions: true,
+          userQuizzAttempts: true,
+        },
+      },
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          image: true,
+          username: true,
+          type: true,
+        },
+      },
+    },
   });
 
   if (!quizz)
     return NextResponse.json({ error: "Quizz not found" }, { status: 404 });
-  return NextResponse.json(quizz, { status: 201 });
+  return NextResponse.json({ quizz } satisfies ReturnSpecificQuizz, {
+    status: 200,
+  });
 }
