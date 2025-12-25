65033426e60c9869659a5c6a30f3eb4921455a2d
arslankaleem7229 <arslankaleem7229@gmail.com>
2025-12-25 20:03:46 +0000

    skip type check in build for perf of jenkins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
   images: {
     remotePatterns: [
       {
@@ -18,6 +19,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   /* config options here */
 };
 
